abfb9c256180870a23f9a5e218a3ea944b809e9e
Diana <dlicon2@miners.utep.edu>
2021-02-08 09:05:44 -0700

    reading charecters in GUI.c

--- src/GUI.c
+++ src/GUI.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 void main(){
-  printf("hello world\n");
+  char a;
+  char b;
+  int ch[100];
+  int i;
+  printf("input a string\n");
+  a=0;
+  i=0;
+  while(a!='\n'){
+    a=getchar();
+    ch[i]=a;
+    i=i+1;
+  }
+  printf("%c%c%c ",ch[0],ch[1],ch[2]);
 }
