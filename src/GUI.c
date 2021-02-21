23b762aecc45b4ff10dc71f074afac730c0dc4bc
Diana <dlicon2@miners.utep.edu>
2021-02-21 13:22:00 -0700

    printing number of words inputed to user

--- src/GUI.c
+++ src/GUI.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+#include "tokenizer.h"
 
 void main(){
   char a;
-  char b;
   char ch[100];
   int i;
-  printf("input a string\n");
-  a=0;
   i=0;
-  while(a!='\n'){
-    a=getchar();
+  printf("input  string\n");
+  while((a=getchar())!='\n'){
     ch[i]=a;
     i=i+1;
+    putchar(a);
+ 
+     
   }
-  a=0;
-  i=0;
-  while(a!='\n'){
-    a=ch[i];
-    printf("%c",a);
-    i=i+1;
-  }
+  ch[i]='\0';
+
+  printf("\nyou entered %d words\n",count_words(ch));
+  
 }
