f9cb95c724249f1f2b242a66cec00fe6ce4a19c8
Diana <dlicon2@miners.utep.edu>
2021-03-11 12:24:16 -0700

    GUI now accepts continous input and prints history

--- src/GUI.c
+++ src/GUI.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 
 void main(){
   char a;
-  char ch[100];
   int i;
-  i=0;
-  printf("input  string\n");
-  while((a=getchar())!='\n'){
-    ch[i]=a;
-    i=i+1;
-    putchar(a);
- 
-     
+  List *history=init_history();
+  Item *temp;
+  while(1){
+    char word[100];
+    i=0;
+    printf("input  string\n");
+    while((a=getchar())!='\n'){
+      word[i]=a;
+      i=i+1;
+      putchar(a);
+    }
+    word[i]='\0';
+    if(word[0]=='q'&& word[1]=='\0'){
+      break;
+    }
+    add_history(history,word);
+    printf("\nyou entered %d words\n",count_words(word));
+    char** tokens=tokenize(word);
+    print_tokens(tokens);
   }
-  ch[i]='\0';
-
-  printf("\nyou entered %d words\n",count_words(ch));
-  char **t;
-  t=tokenize(ch);
-  
-  print_tokens(t);
+  print_history(history);
 }
